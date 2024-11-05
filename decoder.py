fixed overflow issue when masking and removed ddp dependencies from the model

decoder.py
@@ -102,7 +102,7 @@ class DecoderAttention(nn.Module):
             )
         if mask is not None:
             mask = mask.unsqueeze(1)
-            attention_w.masked_fill_(mask==0, -1e9)
+            attention_w.masked_fill_(mask==0, -1e4)
 
         # We can use this for visualization later
         attention_scores = nn.functional.softmax(attention_w, dim=-1, dtype=torch.float32).to(wq.dtype)
@@ -178,5 +178,3 @@ class MultimodalProjector(nn.Module):
     def forward(self, x):
 
         return self.multimodal_proj_layer(x)
-
-

model.py
@@ -14,6 +14,7 @@ class MultimodalCaptionGenerator(nn.Module):
         self.output_projection = nn.Linear(decoder_config.hidden_size, decoder_config.vocab_size)
     
     def _merge_input_embeds(self, image_embeds, text_embeds):
+        
         batch_size = image_embeds.shape[0]
         num_image_tokens = image_embeds.shape[1]
         num_text_tokens = text_embeds.shape[1]
@@ -25,9 +26,9 @@ class MultimodalCaptionGenerator(nn.Module):
 
         # Note that all the features of the padding token is 0 from the Embedding look-up table
         # This mask is of shape [batch_size, num_text_tokens]
-        text_pad_mask = (text_embeds.sum(dim=-1) != 0).bool()
+        text_pad_mask = (text_embeds.sum(dim=-1) != 0).bool().to(image_embeds.device)
         # Now we need the causal mask for the text generation part
-        text_causal_mask = torch.tril(torch.ones((num_text_tokens, num_text_tokens))).bool()
+        text_causal_mask = torch.tril(torch.ones((num_text_tokens, num_text_tokens), device=image_embeds.device)).bool()
 
         attention_mask[:, num_image_tokens:, num_image_tokens:] = text_pad_mask.unsqueeze(-1) & text_causal_mask
         attention_mask = attention_mask.float()
@@ -60,4 +61,3 @@ class MultimodalCaptionGenerator(nn.Module):
         return logits, attention_scores
 
     
-

train.py
@@ -117,7 +117,7 @@ def train_epoch(epoch, model, dataloader, optimizer, scheduler, scaler, device, 
             loss = torch.nn.functional.cross_entropy(
                 logits.view(-1, logits.size(-1)), # reshaping to [batch_sz*seq_len, vocab_size]
                 labels.view(-1), # reshaping to [batch_sz*seq_len]
-                ignore_index=config.pad_token_id
+                ignore_index=config.padding_idx
             )
         
         scaler.scale(loss).backward()
@@ -174,7 +174,7 @@ def load_checkpoint(run_dir, model, optimizer, scheduler, scaler):
     
     if os.path.exists(checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
-        model.module.load_state_dict(checkpoint['model_state_dict'])
+        model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         if scheduler and checkpoint['scheduler_state_dict']:
             scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
@@ -301,7 +301,7 @@ def main():
         training_start_time = time.time()
         
         for epoch in range(start_epoch, decoder_config.num_epochs):
-            
+
             metrics = train_epoch(
                 epoch, model, train_loader, optimizer, 
                 scheduler, scaler, device, global_step, decoder_config
@@ -324,7 +324,7 @@ def main():
             checkpoint = {
                 'epoch': epoch,
                 'global_step': global_step,
-                'model_state_dict': model.module.state_dict(),
+                'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'scheduler_state_dict': scheduler.state_dict() if scheduler else None,
                 'scaler_state_dict': scaler.state_dict(),