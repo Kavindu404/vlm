fc79ed7b3b211c7706c6f7db2e93a0aee9b5df06
Kavindu404 <piyumal.kavindu99@icloud.com>
2024-12-01 23:26:36 -0500

    v31

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ class VisionConfig:
 
     def __init__(self):
         self.patch_sz = 16
-        self.embed_dim = 512
+        self.embed_dim = 384
         self.img_sz = 224
-        self.inter_size = 768
+        self.inter_size = 512
         self.num_heads = 4
-        self.num_encoder_layers = 5
+        self.num_encoder_layers = 6
         self.layer_norm_eps = 1e-6
         self.in_channels = 3
         self.dropout_prob = 0.3
@@ -16,24 +16,24 @@ class MultimodalConfig:
 
     def __init__(self):
 
-        self.project_name = "base_vlm_self_attn_sinPos_val_v31"
+        self.project_name = "base_vlm_self_attn_sinPos_val_v32"
         self.eps = 1e-6
-        self.hidden_size = 768
+        self.hidden_size = 512
         self.seq_len = 392
         self.max_len = 196
-        self.inter_dim = 2048
+        self.inter_dim = 1024
         self.num_heads = 4
         self.vocab_size = 50259
         self.padding_idx = 50258
         self.sos_token_id = 50257
         self.eos_token_id = 50256
-        self.num_decoder_layers = 5
-        self.emb_dim = 512
+        self.num_decoder_layers = 8
+        self.emb_dim = 384
         self.proj_dim = self.hidden_size
         self.num_image_tokens = 196
         self.num_text_tokens = 196
 
-        self.learning_rate = 1e-4
+        self.learning_rate = 1e-5
         self.weight_decay = 0.1
         self.warmup_ratio = 0.1
         self.num_epochs = 2000
